d711a76d5ea96a602c5dfae12125b99eda2b897c
Federico Fernandez <federicofernandeztec@gmail.com>
2022-06-25 04:06:14 -0300

    Se rearmo los 2 Js, se uso importacion de modulo para codigo mas limpio

--- Login.js
+++ Login.js
@@ -1,17 +1,51 @@
 'use strict';
 
+import {dbTransaction} from "./Register.js"
+const $loginButton = document.getElementById("user__or__email")
+const $passButton = document.getElementById("user__pass")
+const $pauseConteiner = document.getElementById("pause__conteiner");
+const $pauseBox = document.getElementById("pause__box");
+const $pauseText = document.getElementById("pause__text");
+$pauseBox.removeChild($pauseText);
+const linkW = "./Register.html"
+
+let isOpenOrClosed = (window)=>{
+    let intervalClear = setInterval(() => {
+        if (window.closed) {
+            $pauseConteiner.classList.remove("pause__conteiner");
+            $pauseBox.classList.remove("pause__box");
+            $pauseBox.removeChild($pauseText)
+            clearInterval(intervalClear)
+        }    
+    }, 500);
+}
+
 document.addEventListener("click", e=>{
-    
     if (e.target.matches("#register")) { 
-        const openW = window.open("./Register.html", "__blank")
-        setInterval(isOpenOrClosed, 500, openW)
+        const openW = window.open(linkW, "__blank");
+        $pauseConteiner.classList.add("pause__conteiner");
+        $pauseBox.classList.add("pause__box");
+        $pauseBox.appendChild($pauseText);
+        isOpenOrClosed(openW);
+    }
+    else if (e.target.matches("#button__Login")) {
+        const promise = new Promise((resolve, reject)=> {
+            if($loginButton.value != "") resolve()
+            else reject("No tiene valor insertar texto")
+        })
+        promise
+            .then((res)=>{
+                console.log("contraseña correcta")
+                
+            })
+            .catch((e)=>{
+                console.log("Error:",e)
+            })
     }
 })
 
 
 
 
-let isOpenOrClosed = (window)=>{
-    if(!window.closed) console.log("abierto")
-    else console.log("se cerro");
-}
+
+
--- Register.js
+++ Register.js
@@ -1,1 +1,36 @@
 'use strict';
+const inputUser = document.getElementById("username");
+const inputPass = document.getElementById("password");
+const inputEmail = document.getElementById("email");
+const pRegister = document.getElementById("parrafoRegister");
+
+const IDBRequest = indexedDB.open("Accounts", 1);
+IDBRequest.addEventListener("upgradeneeded", ()=>{
+    const db = IDBRequest.result;
+    db.createObjectStore("account", {
+        autoIncrement: true
+    });
+})
+
+
+
+document.addEventListener("click", e=>{
+    if (e.target.matches("#register__button")) {
+        e.preventDefault();
+        addObject({Username: inputUser.value, Password: inputPass.value, Email: inputEmail.value});
+        pRegister.textContent = "Successful registration";
+        setTimeout(window.close, 2500);
+    }
+});
+
+const addObject = objeto=> {
+    const objectStore = dbTransaction("readwrite");
+    objectStore.add(objeto);
+}
+
+ export const dbTransaction = (method)=>{
+        const db = IDBRequest.result;
+        const IDBtransaction = db.transaction("account", method);
+        const objectStore = IDBtransaction.objectStore("account");
+        return objectStore;
+    }
