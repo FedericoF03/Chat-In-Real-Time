b14a60c1e188f8986a11170f7c9371423ae2b857
Federico Fernandez <federicofernandeztec@gmail.com>
2022-06-25 23:31:05 -0300

    Se arreglo problema con los import export, aprendiendo su uso a fondo, se hizo el sistema de logueo de forma mas limpia y sencilla, es mejorable pero es mejor mirarlo cuando trabaje en backend

--- Login.js
+++ Login.js
@@ -1,15 +1,15 @@
 'use strict';
 
-import {dbTransaction, IDBRequest} from "./Register.js"
+import { dbTransaction } from "./Register.js";
+
 const $loginButton = document.getElementById("user__or__email");
 const $passButton = document.getElementById("user__pass");
-const $loginText = document.getElementById("login__text")
-const $loginText2 = document.getElementById("login__text2")
+const $loginText = document.getElementById("login__text");
 const $pauseConteiner = document.getElementById("pause__conteiner");
 const $pauseBox = document.getElementById("pause__box");
 const $pauseText = document.getElementById("pause__text");
 $pauseBox.removeChild($pauseText);
-const linkW = "./Register.html"
+const linkW = "./Register.html";
 
 let isOpenOrClosed = (window)=>{
     let intervalClear = setInterval(() => {
@@ -23,55 +23,41 @@ let isOpenOrClosed = (window)=>{
 }
 
 document.addEventListener("click", e=>{
+    
     if (e.target.matches("#register")) { 
         const openW = window.open(linkW, "__blank");
         $pauseConteiner.classList.add("pause__conteiner");
         $pauseBox.classList.add("pause__box");
-        $pauseText.innerHTML = "in registration process"
+        $pauseText.innerHTML = "in registration process";
         $pauseBox.appendChild($pauseText);
         isOpenOrClosed(openW);
     }
     else if (e.target.matches("#button__Login")) {
         if ($loginButton.value != "" || $passButton.value != "") {
-            readObject()
+            readObject();
+        } else {
+            $loginText.style.display = "block";
+            $loginText.innerHTML = "The sent data is empty";
+        }
+    }
+})
+
+document.addEventListener("keydown", (e)=>{
+    if (e.keyCode == 13) {
+        if ($loginButton.value != "" || $passButton.value != "") {
+            readObject();
         } else {
             $loginText.style.display = "block"
-            $loginText.innerHTML = "Your Password or Username/Email is incorrect";
+            $loginText.innerHTML = "The sent data is empty";
         }
     }
 })
 
 const readObject = () =>{
-   const objectStore = dbTransaction("readonly")
+    const objectStore = dbTransaction("readonly");
     const cursor = objectStore.openCursor();
     cursor.addEventListener("success", ()=>{
-        const promise = new Promise((resolve, reject)=>{
-            if($loginButton.value == cursor.result.value.Username || $loginButton.value == cursor.result.value.Email) resolve(cursor.result.value.Password)
-            else if ($loginButton.value != cursor.result.value.Username && $loginButton.value != cursor.result.value.Email) reject()
-        })
-        promise
-                .then(()=>{
-                    passwCheck(cursor.result.value.Password)
-                })
-                .catch(e=>{
-                    passwCheck(cursor.result.value.Password)
-                    $loginText.style.margin = "2px"
-                    $loginText.style.display = "block"
-                    $loginText.innerHTML = "Incorrect Username or Email"})
+        if ((cursor.result.value.Username == $loginButton.value || cursor.result.value.Email == $loginButton.value) && cursor.result.value.Password == $passButton.value) window.open("./Chat.html", "_self");
+        else cursor.result.continue();
     })
-}
-
-const passwCheck = (parametro)=>{
-    const promise = new Promise((resolve, reject) => {
-        if($passButton.value == parametro) resolve()
-        else reject()
-    })
-    promise
-            .then(()=>{
-                window.open("./Chat.html", "_self")
-            })
-            .catch(()=> {
-            $loginText2.style.margin = "2px"
-            $loginText2.style.display = "block"
-            $loginText2.innerHTML = "Incorrect password"})
 }
--- Register.js
+++ Register.js
@@ -1,11 +1,18 @@
-'use strict';
 
+const IDBRequest = indexedDB.open("Accounts", 1);
+
+export const dbTransaction = (method)=>{
+    const db = IDBRequest.result;
+    const IDBtransaction = db.transaction("account", method);
+    const objectStore = IDBtransaction.objectStore("account");
+    return objectStore;
+}
 const inputUser = document.getElementById("username");
 const inputPass = document.getElementById("password");
 const inputEmail = document.getElementById("email");
 const pRegister = document.getElementById("parrafoRegister");
 
-export const IDBRequest = indexedDB.open("Accounts", 1);
+
 IDBRequest.addEventListener("upgradeneeded", ()=>{
     const db = IDBRequest.result;
     db.createObjectStore("account", {
@@ -27,9 +34,3 @@ const addObject = objeto=> {
     objectStore.add(objeto);
 }
 
-export  const dbTransaction = (method)=>{
-        const db = IDBRequest.result;
-        const IDBtransaction = db.transaction("account", method);
-        const objectStore = IDBtransaction.objectStore("account");
-        return objectStore;
-}
